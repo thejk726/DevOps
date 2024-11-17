0cb76567c76681abfc657c2249321f3330575ffd
Thejkiran P <116995836+thejk726@users.noreply.github.com>
2024-11-17 23:44:16 +0530

    Update SpringappApplicationTests.java: explicitly specified class

--- Project/springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
+++ Project/springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=MyApp.class)
 class SpringappApplicationTests {
 
 	@Test
